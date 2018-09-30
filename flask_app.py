f3989ec86c5030f222d5fd2d91dcb3e5f1b17379
Chris Smith <chrisroadmap@gmail.com>
2018-09-30 07:27:22 +0000

    added very basic template

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,11 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'FAIR simple climate model - coming soon?'
+def index():
+    return render_template("main_page.html")
 
