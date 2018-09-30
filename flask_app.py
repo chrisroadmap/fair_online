0873edfe1fab87bf3f32ab4b04663b387c5ab9e2
Chris Smith <chrisroadmap@gmail.com>
2018-09-30 10:50:51 +0000

    Add TCR option

--- flask_app.py
+++ flask_app.py
@@ -3,6 +3,7 @@ from flask_wtf import Form
 from wtforms import FloatField
 from wtforms.validators import NoneOf, NumberRange
 from fair.forward import fair_scm
+#from fair.rcps import rcp26, rcp45, rcp60, rcp85
 import numpy as np
 
 app = Flask(__name__)
@@ -29,14 +30,16 @@ def divide():
 
 @app.route('/fair', methods=['GET', 'POST'])
 def fair():
-    class EcsForm(Form):
+    class FairForm(Form):
         ecs = FloatField("ECS", validators=[NumberRange(min=0.5,max=15)])
+        tcr = FloatField("TCR", validators=[NumberRange(min=0.5,max=10)])
+        # check tcr <= ecs
     
-    form = EcsForm()
+    form = FairForm()
     result = None
     
     if form.validate_on_submit():
-        tcrecs = np.array([1.6, form.ecs.data])
+        tcrecs = np.array([form.tcr.data, form.ecs.data])
         _,_,T = fair_scm(emissions=np.ones(100)*10,
                          useMultigas=False,
                          tcrecs=tcrecs)
