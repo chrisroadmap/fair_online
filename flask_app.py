4e67a362356025a552a0b5db3ef5bae0fa815d10
Chris Smith <chrisroadmap@gmail.com>
2018-09-30 11:43:08 +0000

    Change index link to FAIR

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_wtf import Form
 from wtforms import FloatField
-from wtforms.validators import NoneOf, NumberRange, ValidationError
+from wtforms.validators import NumberRange, ValidationError
 from fair.forward import fair_scm
 #from fair.rcps import rcp26, rcp45, rcp60, rcp85
 import numpy as np
@@ -12,21 +12,6 @@ app.config['SECRET_KEY'] = 'crum47cmcf8zw-00kmxb'
 @app.route('/')
 def index():
     return render_template('index.jinja2')
-
-@app.route('/divide', methods=['GET', 'POST'])
-def divide():
-
-    class DivideForm(Form):
-        number = FloatField("Number")
-        divide_by = FloatField("Divide by", validators=[NoneOf([0])])
-
-    form = DivideForm()
-    result = None
-
-    if form.validate_on_submit():
-        result = form.number.data / form.divide_by.data
-
-    return render_template('divide.jinja2', result=result, form=form)
 
 @app.route('/fair', methods=['GET', 'POST'])
 def fair():
