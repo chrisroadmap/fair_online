a827e1b3b7ce1729d276a128a68a68ea32cdb2b1
Chris Smith <chrisroadmap@gmail.com>
2018-09-30 09:08:02 +0000

    Useful starting point! FAIR runs on PA server with user-input ECS

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 from flask_wtf import Form
 from wtforms import FloatField
-from wtforms.validators import NoneOf
+from wtforms.validators import NoneOf, NumberRange
+from fair.forward import fair_scm
+import numpy as np
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'crum47cmcf8zw-00kmxb'
@@ -25,6 +27,21 @@ def divide():
 
     return render_template('divide.jinja2', result=result, form=form)
 
+@app.route('/fair', methods=['GET', 'POST'])
+def fair():
+    class EcsForm(Form):
+        ecs = FloatField("ECS", validators=[NumberRange(min=0.5,max=15)])
+    
+    form = EcsForm()
+    result = None
+    
+    if form.validate_on_submit():
+        tcrecs = np.array([1.6, form.ecs.data])
+        _,_,T = fair_scm(emissions=np.ones(100)*10,
+                         useMultigas=False,
+                         tcrecs=tcrecs)
+        result = T[-1]
+    return render_template('fair.jinja2', result=result, form=form)
 
 if __name__ == '__main__':
     app.debug = True
